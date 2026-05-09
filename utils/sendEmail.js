c82090abd9e8ebeb241910a665a7e1f2762e988b
shakti-io-1106 <shaktipada.sps@gmail.com>
2026-05-09 17:26:17 +0530

    normal changes

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -31,7 +31,10 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (name, email, message) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      family: 4,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
