cba6337e18157bba172af9a09e5d45b20bb6b508
shakti-io-1106 <shaktipada.sps@gmail.com>
2026-05-09 15:40:50 +0530

    replace gmail with sendgrid

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,35 +1,63 @@
-const nodemailer = require("nodemailer");
+const sgMail = require("@sendgrid/mail");
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendEmail = async (name, email, message) => {
   try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-      tls: {
-        rejectUnauthorized: false,
-      },
-    });
-    const mailOptions = {
-      from: email,
-      to: process.env.EMAIL_USER,
-      subject: `New Contact Form Message from ${name}`,
-      html: `
-                <h3>New Message from Portfolio</h3>
-                <p><b>Name:</b> ${name}</p>
-                <p><b>Email:</b> ${email}</p>
-                <p><b>Message:</b> ${message}</p>
-            `,
+    const msg = {
+      to: process.env.EMAIL_FROM,
+      from: process.env.EMAIL_FROM, // must be verified sender
+      subject: `New Portfolio Message from ${name}`,
+      text: `
+        Name: ${name}
+        Email: ${email}
+        Message: ${message}
+      `,
     };
-    await transporter.sendMail(mailOptions);
+
+    await sgMail.send(msg);
 
     return true;
   } catch (error) {
-    console.log("Email Error:", error);
+    console.log("SendGrid Error:", error.response?.body || error);
     return false;
   }
 };
 
 module.exports = sendEmail;
+
+// const nodemailer = require("nodemailer");
+
+// const sendEmail = async (name, email, message) => {
+//   try {
+//     const transporter = nodemailer.createTransport({
+//       service: "gmail",
+//       auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS,
+//       },
+//       tls: {
+//         rejectUnauthorized: false,
+//       },
+//     });
+//     const mailOptions = {
+//       from: email,
+//       to: process.env.EMAIL_USER,
+//       subject: `New Contact Form Message from ${name}`,
+//       html: `
+//                 <h3>New Message from Portfolio</h3>
+//                 <p><b>Name:</b> ${name}</p>
+//                 <p><b>Email:</b> ${email}</p>
+//                 <p><b>Message:</b> ${message}</p>
+//             `,
+//     };
+//     await transporter.sendMail(mailOptions);
+
+//     return true;
+//   } catch (error) {
+//     console.log("Email Error:", error);
+//     return false;
+//   }
+// };
+
+// module.exports = sendEmail;
