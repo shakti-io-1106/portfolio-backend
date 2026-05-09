2f5e94b981f09888b90963871e7b5cfdc8c53d26
shakti-io-1106 <shaktipada.sps@gmail.com>
2026-05-09 15:55:30 +0530

    managing the .env variables

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -5,8 +5,8 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const sendEmail = async (name, email, message) => {
   try {
     const msg = {
-      to: process.env.EMAIL_FROM,
-      from: process.env.EMAIL_FROM, // must be verified sender
+      to: process.env.ADMIN - EMAIL,
+      from: process.env.EMAIL_FROM,
       subject: `New Portfolio Message from ${name}`,
       text: `
         Name: ${name}
